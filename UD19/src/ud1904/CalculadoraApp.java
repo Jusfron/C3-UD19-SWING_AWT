6ec7e6cedd4c717d01507faef0a05b6c49bdbe7e
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-29 13:50:12 +0200

    created calculadora

--- UD19/src/ud1904/CalculadoraApp.java
+++ UD19/src/ud1904/CalculadoraApp.java
@@ -1,10 +1,20 @@
 package ud1904;
 
+import java.awt.EventQueue;
+
 public class CalculadoraApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculadora frame = new Calculadora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
