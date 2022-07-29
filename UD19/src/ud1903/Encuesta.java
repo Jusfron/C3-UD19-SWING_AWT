c985ceef4477179e701c1011a78200fd92cbdc59
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-29 13:04:45 +0200

    before pull

--- UD19/src/ud1903/Encuesta.java
+++ UD19/src/ud1903/Encuesta.java
@@ -36,45 +36,45 @@ public class Encuesta extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("Windows");
 		rdbtnNewRadioButton.setSelected(true);
 		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setBounds(20, 42, 89, 39);
 		contentPane.add(rdbtnNewRadioButton);
-		
+
 		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Linux");
 		buttonGroup.add(rdbtnNewRadioButton_1);
 		rdbtnNewRadioButton_1.setBounds(122, 42, 77, 39);
 		contentPane.add(rdbtnNewRadioButton_1);
-		
+
 		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Mac");
 		buttonGroup.add(rdbtnNewRadioButton_2);
 		rdbtnNewRadioButton_2.setBounds(211, 42, 64, 39);
 		contentPane.add(rdbtnNewRadioButton_2);
-		
+
 		JLabel lblNewLabel = new JLabel("Elije un sistema operativo");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel.setBounds(20, 11, 218, 39);
 		contentPane.add(lblNewLabel);
-		
+
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Programacion");
 		chckbxNewCheckBox.setBounds(20, 116, 117, 23);
 		contentPane.add(chckbxNewCheckBox);
-		
+
 		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Diseño gráfico");
 		chckbxNewCheckBox_1.setBounds(143, 116, 132, 23);
 		contentPane.add(chckbxNewCheckBox_1);
-		
+
 		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("Administración");
 		chckbxNewCheckBox_2.setBounds(277, 116, 146, 23);
 		contentPane.add(chckbxNewCheckBox_2);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Elije tu especialidad");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_1.setBounds(20, 74, 191, 47);
 		contentPane.add(lblNewLabel_1);
-		
+
 		JSlider slider = new JSlider();
 		slider.setName("");
 		slider.setToolTipText("");
@@ -87,21 +87,21 @@ public class Encuesta extends JFrame {
 		slider.setMaximum(10);
 		slider.setBounds(20, 175, 200, 31);
 		contentPane.add(slider);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Horas dedicadas en el pc");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		lblNewLabel_2.setBounds(20, 146, 309, 23);
 		contentPane.add(lblNewLabel_2);
-		
+
 		JButton ok = new JButton("OK");
 		ok.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, " "+buttonGroup.getSelection().getSelectedObjects()+"!");
+				JOptionPane.showMessageDialog(null, "Sistema:  " + buttonGroup.getSelection().getActionCommand()
+						+ "\nEspecialidad: " + "\nHoras dedicadas: ");
 			}
 		});
 		ok.setBounds(173, 217, 89, 35);
 		contentPane.add(ok);
-		
-		
+
 	}
 }
