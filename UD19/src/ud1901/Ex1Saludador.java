4e59d5dedfde0a47b71d52fdbe35d49c58183efe
Joana <joanajanefdez@gmail.com>
2022-07-29 11:07:28 +0200

    reordenacion codigo

--- UD19/src/ud1901/Ex1Saludador.java
+++ UD19/src/ud1901/Ex1Saludador.java
@@ -1,8 +1,5 @@
 package ud1901;
 
-import java.awt.EventQueue;
-import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,32 +15,37 @@ public class Ex1Saludador extends JFrame {
 	 * Create the frame.
 	 */
 	public Ex1Saludador() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		//config ventana
 		setTitle("Saludador");
+		setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//creacion + config del JPanel
+		contentPane = new JPanel();
 		contentPane.setLayout(null);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		
 		// etiqueta
 		JLabel etiqueta = new JLabel("Escribe tu nombre: ");
 		// colocar etiqueta
 		etiqueta.setBounds(115, 0, 176, 41);
-
 		// añadir etiqueta al panel
 		contentPane.add(etiqueta);
 
-		// input
+		//input
 		JTextField entradaNombre = new JTextField();
 		entradaNombre.setBounds(46, 40, 340, 27);
 		contentPane.add(entradaNombre);
 
-		//boton 
-		
+		//boton 		
 		JButton boton = new JButton("OK");
 		boton.setBounds(151, 89, 117, 25);
 		contentPane.add(boton);
 		
-		setContentPane(contentPane);
+		
 		/*
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.setBounds(113, 86, 117, 25);
